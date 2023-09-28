d534300de5ee6c414d1295c2a03b42149a2cd471
james_hoo <james_hoo@paradise-soft.com.tw>
2023-09-28 19:23:44 +0800

    feat: add 128. Longest Consecutive Sequence

---
+++ array/medium/0128.longest_consecutive_sequence_test.go
@@ -0,0 +1,93 @@
+package medium
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+// method 1
+// 1) use a map to store all nums without duplicate
+// 2) loop the map, find the start of the sequence, and accumulate the length of the sequence into currentLength
+// 3) compare currentLength with maxLength, if currentLength > maxLength, update maxLength
+// 4) return maxLength
+// TC = O(n), SC = O(n)
+// * this is the best solution for me currently
+func longestConsecutive1(nums []int) int {
+	numsMap := make(map[int]bool) // SC = O(n)
+	maxLength := 0
+
+	// put all nums without duplicate into map
+	for _, num := range nums { // TC = O(n)
+		numsMap[num] = true
+	}
+
+	for num := range numsMap { // TC = O(n)
+		// this condition make sure the num is the start of the sequence
+		// if num-1 is in the map, it means the sequence will be start from num-1
+		if !numsMap[num-1] {
+			currentNum := num
+			currentLength := 1
+
+			// find the end of the sequence
+			// this step will not cause the "for num := range numsMap" from O(N) to O(N^2), due to the numbers in the numsMap are unique !!!
+			for numsMap[currentNum+1] {
+				currentNum++
+				currentLength++
+			}
+
+			if currentLength > maxLength {
+				maxLength = currentLength
+			}
+		}
+	}
+
+	return maxLength
+}
+
+func Test_longestConsecutive1(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	type expected struct {
+		result int
+	}
+	type testCase struct {
+		name     string
+		args     args
+		expected expected
+	}
+
+	testCases := []testCase{
+		{
+			name: "1",
+			args: args{
+				nums: []int{100, 4, 200, 1, 3, 2},
+			},
+			expected: expected{
+				result: 4,
+			},
+		},
+		{
+			name: "2",
+			args: args{
+				nums: []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1},
+			},
+			expected: expected{
+				result: 9,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(
+			t,
+			tc.expected.result,
+			longestConsecutive1(tc.args.nums),
+			fmt.Sprintf("testCase name: %s", tc.name),
+		)
+	}
+}
